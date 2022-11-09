d6d6196397630ecc66c71d3f4bd65d4fb2b3bca9
Marcela Trujillo Toro <74881828+marcelaTrujilloToro@users.noreply.github.com>
2022-11-09 14:33:01 -0500

    update row

--- lib/widgets/row.dart
+++ lib/widgets/row.dart
@@ -4,29 +4,42 @@ import 'package:flutter/material.dart';
 class MyRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red.shade300,
-      child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 50,
-              height: 50,
-              color: Colors.green,
-            ),
-            Container(
-              width: 50,
-              height: 50,
-              color: Colors.blue,
-            ),
-            Container(
-              width: 50,
-              height: 100,
-              color: Colors.cyan,
-            ),
-          ],
+    return DefaultTextStyle(
+      style: TextStyle(color: Colors.black),
+      child: Container(
+        color: Colors.red.shade300,
+        alignment:  Alignment.centerLeft,
+        child: SizedBox(
+          width: 300,
+          child: Row(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start ,
+            children: [
+              Container(
+                  width:50,
+                  height: 50,
+                  color: Colors.green,
+                ),
+                Text('lalalalaaskjdfkajshdf asdfadsf afjahsdjf ahsdjhfajksdhf asdfjahsdfkjahsdfk asjdhfkajsdhfkjahs asjhfasjhdf')
+              // Expanded(
+              //   child: Container(
+              //     width: 50,
+              //     height: 50,
+              //     color: Colors.green,
+              //   ),
+              // ),
+              // Container(
+              //   width: 50,
+              //   height: 50,
+              //   color: Colors.blue,
+              // ),
+              // Container(
+              //   width: 50,
+              //   height: 100,
+              //   color: Colors.cyan,
+              // ),
+            ],
+          ),
         ),
       ),
     );
